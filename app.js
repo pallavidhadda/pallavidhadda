0bc623fd9c3e292ff233db9a365d00338e7df955
Pallavi Dhadda <85661960+pallavidhadda@users.noreply.github.com>
2022-05-27 23:06:24 +0530

    trying to resolve name animation glitch

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ $(window).on('load', function () {
     $('.loader-bg').addClass('open');
     $('.loader').removeClass('timetaking');
     $('.loader').addClass('fade-out');
-    $('h1').addClass('name-animation');
 
     setTimeout(function () {
         $("html,body").removeClass("hidden");
     }, 3550);
 });
 $(document).ready(function () {
+    $('h1').addClass('name-animation');
+    
     $("nav a").click(function () {
         $("nav a").removeClass("active");
         $(this).addClass("active");
